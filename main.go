3cf8133b87d5b3fba24591b4892d5f96f9c2fc72
namikaze-dev <usmansadik96@gmail.com>
2023-01-29 17:12:20 +0100

    feat: setup env and loggers

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+var env struct {
+	addr string
+	infoLog  *log.Logger
+	errLog *log.Logger
+}
+
 func main() {
-	
+	// setup env
+	flag.StringVar(&env.addr, "addr", "localhost:8000", "Address of the proxy [default: '127.0.0.1:8000'")
+	flag.Parse()
+
+	env.infoLog = log.New(os.Stdout, "[INFO :]  ", log.Ldate|log.Ltime)
+	env.errLog = log.New(os.Stderr, "[ERROR:]  ", log.Ldate|log.Ltime|log.Lshortfile)
 }
